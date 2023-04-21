30d6b49657407febc38447cb9a669569e9868fc6
gabrielemrt <gabriele.martini.03@gmail.com>
2023-04-21 10:59:24 +0200

    update

---
+++ NodeJS/nodeapp/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+const db_path = path.join(__dirname,'../db/db.json');
+
+router.get('/markers', function(req, res, next) {
+  fs.readFile(db_path, (err, data) => {
+    console.log(data);
+    res.send(JSON.parse(data));
+  });
+
+});
+
+module.exports = router;
