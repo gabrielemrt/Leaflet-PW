b72646e932b8dd7997529a5c1db2db5d218f08c2
gabrielemrt <gabriele.martini.03@gmail.com>
2023-04-25 22:05:54 +0200

    ...

--- NodeJS/nodeapp/app.js
+++ NodeJS/nodeapp/app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/form', form);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- NodeJS/nodeapp/form/form.js
+++ NodeJS/nodeapp/form/form.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
+
 const server = http.createServer((req, res) => {
   if (req.url === '/form' && req.method === 'GET') {
     
