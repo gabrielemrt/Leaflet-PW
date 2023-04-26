637f8caefae1438e93114f46077e52b5835ec367
gabrielemrt <gabriele.martini.03@gmail.com>
2023-04-27 00:38:14 +0200

    update decimal

--- NodeJS/database/init.sql
+++ NodeJS/database/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS progetti (
 	nome_progetto varchar(255) unique,
 	data_inizio_progetto date,
 	data_fine_progetto date,
-	latitudine decimal(10,17),
-	longitudine decimal(10,17),
+	latitudine decimal(20,18),
+	longitudine decimal(20,18),
 	note varchar(1000)
 	);
